51418ce65132fd9f5e84a8fd20e9805a3fcc93fc
Matthias Wirth <matthias.wirth@gmail.com>
2024-09-18 12:24:01 +0200

    more aggressive zram
    use at least 256M of memory for zram backing
    
    bigger uncompressed disksize
    
    switch from lz4 to zstd
    
    disable file based swap

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/zram-swap.sh
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/zram-swap.sh
@@ -13,23 +13,34 @@ if [[ -e /dev/zram0 ]]; then
 fi
 
 modprobe zram
-echo lz4 > /sys/block/zram0/comp_algorithm
+# Compressor            Ratio   Compression Decompression
+# LZ4 default (v1.9.0)  2.101   780 MB/s    4970 MB/s
+# Zstandard 1.4.0 -1    2.883   515 MB/s    1380 MB/s
+echo zstd > /sys/block/zram0/comp_algorithm
 
 # user max quarter of memory
 use=$(( $(grep -e MemTotal /proc/meminfo | tr -s ' ' | cut -d' ' -f2) / 4 ))
 # never more than 1G
-maxkb=$(( 1 * 1024 * 1024 ))
+max=$(( 1 * 1024 * 1024 ))
+# always use at least 256M as zram memory limit
+min=$(( 256 * 1024 ))
 
-if (( use > maxkb )); then
-    size=$maxkb
+if (( use < min )); then
+    size=$min
+elif (( use > maxkb )); then
+    size=$max
 else
     size=$use
 fi
 
 # disk size
-echo $(( 2 * size ))K > /sys/block/zram0/disksize
+# let's make this 3x the memory limit in case we get good compression
+echo $(( 4 * size ))K > /sys/block/zram0/disksize
 # max memory usage
 echo $(( size ))K > /sys/block/zram0/mem_limit
 
 mkswap /dev/zram0
 swapon -p 100 /dev/zram0
+
+# turn off file based swap
+swapoff $(cat /proc/swaps | grep -F -v -e zram -e Filename | cut -d' ' -f1)
